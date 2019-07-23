4c929ea97c263e1a502752ffca069a61c5180939
Louise Song <louise.s.song+github@gmail.com>
2019-07-22 23:06:04 -0400

    Added game json

--- api/poker_logic/Server.cpp
+++ api/poker_logic/Server.cpp
@@ -12,11 +12,81 @@ enum game_type {
   plo
 };
 
+string game_type_toString(game_type type){
+  if(type == nlhe){
+    return "nlhe";
+  } else {
+    return "plo";
+  }
+}
+
 enum game_format {
   ring,
   sitngo,
   tournament
 };
+
+string game_format_toString(game_format format){
+  if(format == ring){
+    return "ring";
+  } else if (format == sitngo){
+    return "sitngo";
+  } else if (format == tournament){
+    return "tournament";
+  }
+}
+
+string json_intarray(const vector<int>& array) {
+  //no trailing comma
+  string s = "";
+  s += "[";
+  if(array.size() > 0){
+    for(int i = 0; i < array.size() - 1; i++){
+      s += to_string(array[i]) + ", ";
+    }
+    s += to_string(array[array.size() - 1]);
+  }
+  s += "]";
+  return s;
+}
+
+string json_stringarray(const vector<string>& array) {
+  string s = "";
+  s += "[";
+  if(array.size() > 0){
+    for(int i = 0; i < array.size() - 1; i++){
+      s += "\"" + array[i] + "\", ";
+    }
+    s += "\"" + array[array.size() - 1] + "\"";
+  }
+  s += "]";
+  return s;
+}
+
+string intvalue(string key, int value, int numtab, bool hascomma){
+  string s = "";
+  for(int i = 0; i < numtab; i++){
+    s += "\t";
+  }
+  s += "\"" + key + "\": " + to_string(value);
+  if(hascomma){
+    s += ",";
+  }
+  s += "\n";
+}
+
+string stringvalue(string key, string value, int numtab, bool hascomma){
+  value = "\"" + value + "\"";
+  string s = "";
+  for(int i = 0; i < numtab; i++){
+    s += "\t";
+  }
+  s += "\"" + key + "\": " + value;
+  if(hascomma){
+    s += ",";
+  }
+  s += "\n";
+}
 
 struct Game {
   string id;
@@ -27,6 +97,19 @@ struct Game {
   int big_blind;
   int blind_timer;
   vector<string> tables;
+
+  string output_game(){
+    string s = "{\n";
+    s += stringvalue("d", id, 1, true);
+    s += stringvalue("type", game_type_toString(type), 1, true);
+    s += stringvalue("format", game_format_toString(format), 1, true);
+    s += intvalue("table_size", table_size, 1, true);
+    s += intvalue("buy_in", buy_in, 1, true);
+    s += intvalue("big_blind", big_blind, 1, true);
+    s += intvalue("blind_timer", blind_timer, 1, true);
+    s += stringvalue("tables", json_stringarray(tables), 1, false);
+    s += "}";
+  }
 };
 
 struct Table {
@@ -34,20 +117,6 @@ struct Table {
   string game_id;
   vector<string> player_ids;
   HandSimulation hand_sim;
-
-  string json_intarray(const vector<int>& array) {
-    //no trailing comma
-    string s = "";
-    s += "[";
-    if(array.size() > 0){
-      for(int i = 0; i < array.size() - 1; i++){
-        s += to_string(array[i]) + ", ";
-      }
-      s += to_string(array[array.size() - 1]);
-    }
-    s += "]";
-    return s;
-  }
 
   string output_table(){
     string s = "{\n";
@@ -61,16 +130,12 @@ struct Table {
       s += "\t\t\"" + player_ids.back() + "\"\n";
     }
     s += "\t],\n";
-    s += "\t\"board\": [";
-    const vector<Card>& board = hand_sim.getBoard();
-    if(board.size() > 0){
-      for(int i = 0; i < board.size() - 1; i++){
-        s += "\"" + board[i].ShowCard() + "\", ";
-      }
-      s += "\"" + board[board.size() - 1].ShowCard() + "\"";
+    vector<Card> board = hand_sim.getBoard();
+    vector<string> s_board;
+    for(int i = 0; i < board.size(); i++){
+      s_board.push_back(board[i].ShowCard());
     }
-    s += "],\n";
-    //need to implement get methods for hand_sim and change current
+    stringvalue("board", json_stringarray(s_board), 1, true);
     //variable camelCase to snake_case
     s += "\t\"pots\": " + json_intarray(hand_sim.getPots()) + ",\n";
     s += "\t\"bets\": " + json_intarray(hand_sim.getBets()) + ",\n";
@@ -124,10 +189,10 @@ int main(){
     string table_id = request->path_match[1].str();
     string res;
     all_tables_mutex.lock();
-    if(all_tables.count(table_id)){
+    if(all_tables.count(table_id) > 0){
       table_mutexes[table_id]->lock();
-      Table& thetable = *all_tables[table_id];
-      res = thetable.output_table();
+      Table&table = *all_tables[table_id];
+      res = table.output_table();
       table_mutexes[table_id]->unlock();
     } else {
       res = "{\n";
@@ -140,5 +205,30 @@ int main(){
               << res;
     all_tables_mutex.unlock();
   };
+
+  server.resource["^/api/game/([0-9]+)$"]["GET" ] = [](
+    shared_ptr<HttpServer::Response> response,
+    shared_ptr<HttpServer::Request> request
+  ) {
+    string game_id = request->path_match[1].str();
+    string res;
+    all_games_mutex.lock();
+    if(all_games.count(game_id) > 0){
+      game_mutexes[game_id]->lock();
+      Game& game = *all_games[game_id];
+      res = game.output_game();
+      game_mutexes[game_id]->unlock();
+    } else {
+      res = "{\n";
+      res += "\t\"error\": \"Game ID Not Found\"\n";
+      res += "}\n";
+    }
+
+    *response << "HTTP/1.1 200 OK\r\n"
+              << "Content-Length: " << res.size() << "\r\n\r\n"
+              << res;
+    all_games_mutex.unlock();
+  };
+
   server.start();
 }
